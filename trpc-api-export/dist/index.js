478d7805a0c8940c9033ff2532d5223b9eb4ad96
mkosir <kosir.mar@gmail.com>
2023-06-12 14:03:25 +0200

    fix(deps): update dependencies

--- trpc-api-export/dist/index.js
+++ trpc-api-export/dist/index.js
@@ -1,49 +1,52 @@
-"use strict";
+'use strict';
 var __defProp = Object.defineProperty;
 var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
 var __getOwnPropNames = Object.getOwnPropertyNames;
 var __hasOwnProp = Object.prototype.hasOwnProperty;
 var __export = (target, all) => {
-  for (var name in all)
-    __defProp(target, name, { get: all[name], enumerable: true });
+  for (var name in all) __defProp(target, name, { get: all[name], enumerable: true });
 };
 var __copyProps = (to, from, except, desc) => {
-  if (from && typeof from === "object" || typeof from === "function") {
+  if ((from && typeof from === 'object') || typeof from === 'function') {
     for (let key of __getOwnPropNames(from))
       if (!__hasOwnProp.call(to, key) && key !== except)
-        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+        __defProp(to, key, {
+          get: () => from[key],
+          enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable,
+        });
   }
   return to;
 };
-var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+var __toCommonJS = (mod) => __copyProps(__defProp({}, '__esModule', { value: true }), mod);
 
 // trpc-api-export/builder/index.ts
 var builder_exports = {};
 __export(builder_exports, {
   SharedSquare: () => SharedSquare,
-  USER_ROLES: () => USER_ROLES
+  USER_ROLES: () => USER_ROLES,
 });
 module.exports = __toCommonJS(builder_exports);
 
 // src/db/User.ts
-var import_zod = require("zod");
-var USER_ROLES = ["administrator", "apprentice", "standard"];
+var import_zod = require('zod');
+var USER_ROLES = ['administrator', 'apprentice', 'standard'];
 var UserConfigSchema = import_zod.z.object({
   id: import_zod.z.string(),
   email: import_zod.z.string(),
   name: import_zod.z.string(),
   username: import_zod.z.string(),
   imageUrl: import_zod.z.string().optional(),
-  role: import_zod.z.enum(USER_ROLES)
+  role: import_zod.z.enum(USER_ROLES),
 });
 
 // trpc-api-export/builder/index.ts
 var SharedSquare = {
-  shape: "square",
-  size: 50
+  shape: 'square',
+  size: 50,
 };
 // Annotate the CommonJS export names for ESM import in node:
-0 && (module.exports = {
-  SharedSquare,
-  USER_ROLES
-});
+0 &&
+  (module.exports = {
+    SharedSquare,
+    USER_ROLES,
+  });
