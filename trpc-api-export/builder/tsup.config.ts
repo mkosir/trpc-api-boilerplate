b543a92a73c071b2014dffc802de5613275b56a3
mkosir <kosir.mar@gmail.com>
2023-12-09 19:01:17 +0100

    fix(deps): update dependencies

--- trpc-api-export/builder/tsup.config.ts
+++ trpc-api-export/builder/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup';
 const tsupConfig = defineConfig({
   entry: ['trpc-api-export/builder/index.ts'],
   outDir: 'trpc-api-export/dist',
-  format: ['cjs'],
+  format: ['esm'],
   clean: true,
   dts: true,
   tsconfig: 'trpc-api-export/builder/tsconfig.tsup.json',
